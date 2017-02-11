ddd95ec041b492d19519baa93f0d742786ed2d53
veyndan <veyndan89@gmail.com>
2017-02-11 20:47:25 +0000

    Rename NumberStringUtilsTest to BaseTest

--- app/src/test/java/com/akseltorgard/devcalc/BaseTest.java
+++ app/src/test/java/com/akseltorgard/devcalc/BaseTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class NumberStringUtilsTest {
+public class BaseTest {
 
     @Test
     public void testIntToBinaryString() {
